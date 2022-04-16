0d9b936459311725ed8f605e623ceb91ebbb18dc
Guilherme Abacherli <gpabacherli@gmail.com>
2022-04-15 21:15:04 -0300

    inf335, trabalho2, tests improved

--- inf335/trabalho2/src/test/java/br/unicamp/ic/inf335/beans/AnuncianteBeanTest.java
+++ inf335/trabalho2/src/test/java/br/unicamp/ic/inf335/beans/AnuncianteBeanTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 /**
@@ -12,16 +14,60 @@ import org.junit.Test;
  */
 public class AnuncianteBeanTest {
 
-  AnuncianteBean anunciante = new AnuncianteBean();
-  AnuncioBean anuncio = new AnuncioBean();
+  @Test
+  public void testGettersAndSetters() {
+    AnuncianteBean anunciante = new AnuncianteBean();
+
+    String expectedNome = "John Doe";
+    String expectedCPF = "498.424.490-92";
+
+    ArrayList<AnuncioBean> expectedAnuncios = new ArrayList<AnuncioBean>();
+    for (int i = 0; i < 5; i++) {
+      expectedAnuncios.add(new AnuncioBean());
+    }
+
+    anunciante.setNome(expectedNome);
+    anunciante.setCPF(expectedCPF);
+    anunciante.setAnuncios(expectedAnuncios);
+
+    assertEquals(expectedNome, anunciante.getNome());
+    assertEquals(expectedCPF, anunciante.getCPF());
+    assertEquals(expectedAnuncios, anunciante.getAnuncios());
+  }
 
   @Test
-  public void testConstructor() {
-    anunciante = new AnuncianteBean();
+  public void testConstructorEmpty() {
+    AnuncianteBean anunciante = new AnuncianteBean();
+    assertEquals("", anunciante.getNome());
+    assertEquals("", anunciante.getCPF());
+    assertTrue(anunciante.getAnuncios().isEmpty());
+    assertTrue(anunciante.getAnuncios().size() == 0);
+  }
+
+  @Test
+  public void testConstructorWithParams() {
+    AnuncianteBean anunciante = new AnuncianteBean();
+
+    String expectedNome = "John Doe";
+    String expectedCPF = "498.424.490-92";
+
+    ArrayList<AnuncioBean> anuncios = new ArrayList<AnuncioBean>();
+    for (int i = 0; i < 5; i++) {
+      anuncios.add(new AnuncioBean());
+    }
+
+    anunciante = new AnuncianteBean(expectedNome, expectedCPF, anuncios);
+
+    assertEquals(expectedNome, anunciante.getNome());
+    assertEquals(expectedCPF, anunciante.getCPF());
+    assertTrue(anunciante.getAnuncios().size() == 5);
   }
 
   @Test
   public void testAddAnuncio() {
+    AnuncianteBean anunciante = new AnuncianteBean();
+    AnuncioBean anuncio = new AnuncioBean();
+
     assertTrue(anunciante.getAnuncios().isEmpty());
     assertTrue(anunciante.getAnuncios().size() == 0);
 
@@ -32,6 +78,9 @@ public class AnuncianteBeanTest {
 
   @Test
   public void testRemoveAnuncio() {
+    AnuncianteBean anunciante = new AnuncianteBean();
+    AnuncioBean anuncio = new AnuncioBean();
+
     assertTrue(anunciante.getAnuncios().isEmpty());
     assertTrue(anunciante.getAnuncios().size() == 0);
 
@@ -46,6 +95,8 @@ public class AnuncianteBeanTest {
 
   @Test
   public void testValorMedioAnuncios() {
+    AnuncianteBean anunciante = new AnuncianteBean();
+
     AnuncioBean anuncio1 = new AnuncioBean();
     anuncio1.getProduto().setValor(6.0);
     anuncio1.setDesconto(0.5);
@@ -58,7 +109,7 @@ public class AnuncianteBeanTest {
     anunciante.addAnuncio(anuncio2);
     assertTrue(anunciante.getAnuncios().size() == 2);
 
-    Double expected = Double.valueOf(4.5);
+    Double expected = 4.5;
     Double actual = anunciante.valorMedioAnuncios();
     assertEquals(expected, actual);
   }
